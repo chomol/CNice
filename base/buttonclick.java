e8d009abe705c139b632d5dc807c1877ce512009
chomol <chomobaikal@gmail.com>
2014-08-24 03:11:40 +0800

    ysdf

---
+++ base/buttonclick.java
@@ -0,0 +1,35 @@
+
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //得到按钮实例
+        Button hellobtn = (Button)findViewById(R.id.hellobutton);
+        //设置监听按钮点击事件
+        hellobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //得到textview实例
+                TextView hellotv = (TextView)findViewById(R.id.hellotextView);
+                //弹出Toast提示按钮被点击了
+                Toast.makeText(MyActivity.this,"Clicked",Toast.LENGTH_SHORT).show();
+                //读取strings.xml定义的interact_message信息并写到textview上
+                hellotv.setText(R.string.interact_message);
+            }
+        });
+    }
+
+}
